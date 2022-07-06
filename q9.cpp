aee300e3bbf8eed358c29d63463c5552fd0ffe79
Aryan Sethi <aryan.rdps@gmail.com>
2022-07-06 16:05:35 +0530

    Added q9 as done

---
+++ q9.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <ctime>
+#include <fstream>
+using namespace std;
+
+class Array
+{
+    int n, a, b;
+    int *arr;
+
+public:
+    Array()
+    {
+        n = 0;
+        a = 0;
+        b = 0;
+    }
+
+    Array(int n, int a, int b)
+    {
+        this->n = n;
+        this->a = a;
+        this->b = b;
+        arr = new int[n];
+    }
+
+    ~Array()
+    {
+        delete[] arr;
+    }
+
+    void setArray()
+    {
+        for (int i = 0; i < n; i++)
+            arr[i] = randomNumber();
+    }
+
+    int randomNumber()
+    {
+        int r = (rand() % (b - a)) + a;
+        return r;
+    }
+
+    void display()
+    {
+        for (int i = 0; i < n; i++)
+            cout << arr[i] << " ";
+        cout << endl;
+    }
+
+    int insertionSort()
+    {
+        int comparisons = 0;
+        int key , j;
+        for (int i = 1; i < n; i++)
+        {
+            key = arr[i];
+            j = i - 1;
+            while(j >= 0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+                comparisons++;
+            }
+            arr[j+1] = key;
+        }
+        return comparisons;
+    }
+};
+
+int main()
+{
+
+    ofstream out("q9.csv");
+    srand((unsigned)time(NULL));
+    int n, a, b;
+    n = 10;
+    a = 1;
+    b = 20;
+    Array arr;
+
+    int comparisons;
+
+    for (int i = 1; i <= 250; i+=5)
+    {
+        arr = Array(i, a, b);
+        arr.setArray();
+        comparisons = arr.insertionSort();
+        cout << "n : " << i << " Comparisons : " << comparisons << endl;
+        out << i << "," << comparisons << endl;
+    }
+    out.close();
+
+    return 0;
+}
