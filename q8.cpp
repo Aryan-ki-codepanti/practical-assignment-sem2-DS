3cdc9b862e7d9eee626cf2ea4bdbed645b0c48d1
Aryan Sethi <aryan.rdps@gmail.com>
2022-07-06 15:39:23 +0530

    Q8 to be uploaded

---
+++ q8.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+class Array
+{
+    int n, a, b;
+    int *arr;
+
+public:
+    Array()
+    {
+        n = 0;
+        a = 0;
+        b = 0;
+    }
+
+    Array(int n, int a, int b)
+    {
+        this->n = n;
+        this->a = a;
+        this->b = b;
+        arr = new int[n];
+    }
+
+    ~Array()
+    {
+        delete[] arr;
+    }
+
+    void setArray()
+    {
+        for (int i = 0; i < n; i++)
+            arr[i] = randomNumber();
+    }
+
+    int randomNumber()
+    {
+        int r = (rand() % (b - a)) + a;
+        return r;
+    }
+
+    void display()
+    {
+        for (int i = 0; i < n; i++)
+            cout << arr[i] << " ";
+        cout << endl;
+    }
+
+    int bubbleSort()
+    {
+        int comparisons = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    swap(arr[j], arr[j + 1]);
+                    comparisons++;
+                }
+            }
+        }
+        return comparisons;
+    }
+};
+
+int main()
+{
+
+    srand((unsigned)time(NULL));
+    int n, a, b;
+    n = 10;
+    a = 1;
+    b = 20;
+    Array arr;
+
+    int comparisons;
+    for (int i = 1; i <= 250; i += 5)
+    {
+        arr = Array(i, a, b);
+        arr.setArray();
+        // cout << "Original : ";
+        // arr.display();
+
+        comparisons = arr.bubbleSort();
+        // cout << "Sorted : ";
+        // arr.display();
+        cout << "n : " << i << " Comparisons : " << comparisons << endl;
+    }
+
+    return 0;
+}
